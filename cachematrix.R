d03b600ff44ad487184b865038ae67ce9abd0ee7
sallentoo <sheng.tu@hotmail.com>
2015-01-24 21:51:11 -0500

    R Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL 
+      }
+      get <- function() x
+      setinv <- function(inv) m <<-inv
+      getinv <- function() m
+      list(set=set, get=get,
+           setinv=setinv,
+           getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. It the inverse has already been calculated 
+## (and the matrix has not changed), then the cachesolve should retrieve 
+## the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+      ## Return a matrix that is the inverse of 'x'
+      
+      start_time<-Sys.time()
+      
+      m <- x$getinv()
+      if(!is.null(m)) {
+            message("getting cached data")
+            
+            end_time<-Sys.time()
+            time_duration<-end_time-start_time
+            print(time_duration)
+            
+            return(m)
+      }
+      
+      data <- x$get()
+      m <- solve(data)
+      x$setinv(m)
+      
+      end_time<-Sys.time()
+      time_duration<-end_time-start_time
+      print(time_duration)
+      
+      m
 }
